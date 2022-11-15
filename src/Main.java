e60207c107ac0b7c0eeacdbf93b0750351f1e1cc
patipan-patisampita <patipan.patisampita@gmail.com>
2022-11-15 14:57:41 +0700

    update

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,9 @@ public class Main {
         Dog Dog1Object = new Dog();
         Dog Dog2Object = new Dog();
         Dog Dog3Object = new Dog();
+
+        Dog1Object.testDog();
+        Dog2Object.testDog2();
+        Dog3Object.testDog3();
     }
 }
